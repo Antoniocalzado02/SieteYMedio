0ee4ce8336901d93cb1550c990157c4ffae51676
Antoniocalzado02 <antoniocalzado02@gmail.com>
2022-03-02 13:21:36 +0100

    Nuevo directorio de github

---
+++ neweclipse-workspace/sieteymedia/src/com/main/SieteYMediaMain.java
@@ -0,0 +1,40 @@
+package com.main;
+
+import java.util.Scanner;
+
+import com.model.Baraja;
+import com.model.Carta;
+import com.model.Palo;
+
+public class SieteYMediaMain {
+
+	public static void main(String[] args) {
+		//Scanner sc= new Scanner(System.in);
+		//System.out.println("Dime el palo");
+		
+		//Palo nuevoPalo=Palo.valueOf(sc.nextLine());
+		//System.out.println(nuevoPalo);
+		
+		//System.out.println(Palo.values()[3]);
+		
+		//new Baraja();
+		new Baraja();
+		Baraja b1=new Baraja();
+		b1.barajar();
+		//System.out.println(b1.toString());
+		
+		//Baraja b2=new Baraja();
+		//int coincidencias=0;
+		
+		//for(int i =0; i<b2.getListaCartas().length;i++) {
+			//if(b1.getListaCartas()[i].equals(b2.getListaCartas()[i])) {
+				//coincidencias++;
+			//}
+		//}
+		//System.out.println(coincidencias);
+		
+		Baraja[] listaCartas=new Baraja[];
+
+	}
+
+}
